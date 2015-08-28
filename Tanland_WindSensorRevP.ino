a126cf14100ea3a9d9f1ca81e04c3aa1a0da6404
Benjamin Low <lthben@gmail.com>
2015-08-28 18:39:38 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Tanland_WindSensorRevP.ino

--- Tanland_WindSensorRevP.ino
+++ Tanland_WindSensorRevP.ino
@@ -4,20 +4,20 @@
 * for the Modern Device Rev P Wind Sensor
 * Requires a Wind Sensor Rev P from Modern Device
 * http://moderndevice.com/product/wind-sensor-rev-p/
-* 
-* The Rev P requires at least at least an 8 volt supply. The easiest way to power it 
+*
+* The Rev P requires at least at least an 8 volt supply. The easiest way to power it
 * if you are using an Arduino is to use a 9 volt or higher supply on the external power jack
 * and power the sensor from Vin.
-* 
-* Hardware hookup 
+*
+* Hardware hookup
 * Sensor     Arduino Pin
 * Ground     Ground
 * +10-12V      Vin
 * Out          A0
 * TMP          A2
-* 
-* Adapted for project Tanland in Ayutthaya, Thailand, for the heat wave 
-* installation. Serial writes a byte value 0f 0-255 for the wind sensor. 
+*
+* Adapted for project Tanland in Ayutthaya, Thailand, for the heat wave
+* installation. Serial writes a byte value 0f 0-255 for the wind sensor.
 * A higher reading means stronger airflow.
 */
 
@@ -26,11 +26,22 @@ const int TempPin = A2;   // temp sesnsor analog pin hooked up to Wind P sensor 
 
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
 
+<<<<<<< HEAD
+  // read wind
+  int windADunits = analogRead(OutPin);
+  //    Serial.print("RW ");   // print raw A/D for debug
+  //    Serial.print(windADunits);
+  //    Serial.print("\t");
+
+  int my_reading = windADunits - 300;
+  my_reading = constrain(my_reading, 0, 255);
+  Serial.println(my_reading); //raw reading is 330-500. To keep to within one byte 0-255.
+=======
     // read wind
     int windADunits = analogRead(OutPin);
     
@@ -51,8 +62,32 @@ void loop() {
     float windMPH =  pow((((float)windADunits - 264.0) / 85.6814), 3.36814);
 //    Serial.print(windMPH);
 //    Serial.print(" MPH\t");    
+>>>>>>> origin/master
 
+  // wind formula derived from a wind tunnel data, annemometer and some fancy Excel regressions
+  // this scalin doesn't have any temperature correction in it yet
+  float windMPH =  pow((((float)windADunits - 264.0) / 85.6814), 3.36814);
+  //    Serial.print(windMPH);
+  //    Serial.print(" MPH\t");
 
+<<<<<<< HEAD
+
+  // temp routine and print raw and temp C
+  int tempRawAD = analogRead(TempPin);
+  //    Serial.print("RT ");    // print raw A/D for debug
+  //    Serial.print(tempRawAD);
+  //    Serial.print("\t");
+
+  // convert to volts then use formula from datatsheet
+  //    Vout = ( TempC * .0195 ) + .400
+  //    tempC = (Vout - V0c) / TC   see the MCP9701 datasheet for V0c and TC
+
+  float tempC = ((((float)tempRawAD * 5.0) / 1024.0) - 0.400) / .0195;
+  //    Serial.print(tempC);
+  //    Serial.println(" C");
+
+  delay(50); //need to add delay for the sampling to work
+=======
     // temp routine and print raw and temp C
     int tempRawAD = analogRead(TempPin);  
 //    Serial.print("RT ");    // print raw A/D for debug
@@ -70,6 +105,10 @@ void loop() {
     
     delay(10);
     
+<<<<<<< Updated upstream
+=======
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 }
 
 
